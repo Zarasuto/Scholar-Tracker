492b8dd2386e6d3112d276414a2ab52d46c9f3f1
zarasuto <zarasuto@gmail.com>
2021-08-04 15:30:00 +0800

    added bot entry point added bareback command file added JSON file to store names and their ronin addresses

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '?')
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(f"Ping : {round(client.latency * 1000)}ms ")
+
+client.load_extension('commands')
+
+client.run("ODcyMTk4MDY4MjkwODc5NTU4.YQmXwA.q5ckFJLRLRKWIB2EHNkOdLeWDsI")
